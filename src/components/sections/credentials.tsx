42fa20371d4d4456b990d0063cd8a98dcf41bc00
ahmadssadiq <asadiq456@outlook.com>
2025-09-21 20:31:52 -0400

    made the cliclable words more noticable)

--- src/components/sections/credentials.tsx
+++ src/components/sections/credentials.tsx
@@ -15,9 +15,9 @@ const CredentialsSection = ({ showTennis, showBoston, setShowBoston, setShowTenn
       <p className={`mb-1 text-sm font-normal transition-colors duration-300 ${(showTennis || showBoston) ? 'text-gray-300' : 'text-muted'}`}>
         / <button onClick={() => { setShowBoston(!showBoston); setShowTennis(false); setShowPakistan(false); setShowQatar(false); }} className={`text-sm transition-all duration-300 ${showBoston ? 'text-green-500' : 'bg-gradient-to-r from-[#dc2626] to-[#fca5a5] bg-clip-text text-transparent hover:opacity-80 cursor-pointer'}`} style={{fontSize: 'inherit'}}>Boston University</button> Computer Science (May 2025)
       </p>
-      {/* <p className={`m-0 text-sm font-normal transition-colors duration-300 ${(showTennis || showBoston) ? 'text-gray-300' : 'text-muted'}`}>
-        / Available for opportunities 
-      </p> */}
+      <p className={`m-0 text-sm font-normal transition-colors duration-300 ${(showTennis || showBoston) ? 'text-gray-300' : 'text-muted'}`}>
+        / Click the bold words
+      </p>
     </div>
   );
 };
